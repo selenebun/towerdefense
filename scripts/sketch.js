858f46c585b949ed4996026746d0c11a5d1130cc
xithiox <xithiox@gmail.com>
2017-12-04 18:56:03 -0600

    Reworked tile system, finished pathing

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,64 +1,140 @@
 var enemies;
-var enemiesStack;
-var grid;
+var newEnemies;
+var towers;
+var newTowers;
 
-var tWidth;
-var tHeight;
+var grid;
+var paths;
+var spawnpoints;
+var exit;
+
+var minCols = 30;
+var minRows = 20;
 var cols = 40;
 var rows = 25;
+var tw;             // Tile width
+var th;             // Tile height
 
 var paused;
-
 var maxHealth;
 var health;
 var cash;
 var wave;
 
-var exitCount = 1;
-var showGrid = true;
-var spawnerCount = 1;
-var wallCover = 0.1;
+var gridMode = true;
+var pathMode = false;
+var spawnCount = 1;
+var wallChance = 0.1;
 
 
 // Misc functions
 
 function generateMap() {
-    // Generate basic tiles and walls
+    // Generate empty tiles and walls
     grid = [];
     for (var col = 0; col < cols; col++) {
         grid[col] = [];
         for (var row = 0; row < rows; row++) {
-            if (random() < wallCover) {
-                grid[col][row] = createTile(col, row, tiles.wall);
-            } else {
-                grid[col][row] = createTile(col, row, tiles.empty);
+            grid[col][row] = random() < wallChance ? 1 : 0;
+        }
+    }
+
+    // Generate exit
+    exit = getEmpty(grid);
+
+    // Generate enemy spawnpoints
+    spawnpoints = [];
+    for (var i = 0; i < spawnCount; i++) {
+        var pos;
+        while (true) {
+            pos = getEmpty(grid);
+            if (pos.x !== exit.x && pos.y !== exit.y) break;
+        }
+        spawnpoints.push(pos);
+    }
+
+    generatePaths(grid, exit.x, exit.y);
+}
+
+// Generates shortest path to target tile from every map tile
+function generatePaths(walkMap, col, row) {
+    var frontier = new Queue();
+    var target = cts(col, row);
+    frontier.enqueue(target);
+    var cameFrom = {};
+    cameFrom[target] = null;
+
+    // Fill cameFrom for every tile
+    while (!frontier.isEmpty()) {
+        var current = frontier.dequeue();
+        var pos = stc(current);
+        var neighbors = getNeighbors(walkMap, pos.x, pos.y);
+
+        for (var i = 0; i < neighbors.length; i++) {
+            var next = neighbors[i];
+            if (!(next in cameFrom)) {
+                frontier.enqueue(next);
+                cameFrom[next] = current;
             }
         }
     }
 
-    // Generate spawners
-    for (var i = 0; i < spawnerCount; i++) {
-        var col = floor(random(cols));
-        var row = floor(random(rows));
-        grid[col][row] = createTile(col, row, tiles.spawner);
-    }
+    // Generate path direction for every tile
+    paths = buildMap(cols, rows, null);
+    var keys = Object.keys(cameFrom);
+    for (var i = 0; i < keys.length; i++) {
+        var curKey = keys[i];
+        var curVal = cameFrom[curKey];
 
-    // Generate exits
-    for (var i = 0; i < exitCount; i++) {
-        var col = floor(random(cols));
-        var row = floor(random(rows));
-        grid[col][row] = createTile(col, row, tiles.exit);
+        if (curKey === null || curVal === null) continue;
+        var current = stv(curKey);
+        var next = stv(curVal);
+        var dir = next.sub(current);
+        if (dir.x < 0) paths[current.x][current.y] = 'left';
+        if (dir.y < 0) paths[current.x][current.y] = 'up';
+        if (dir.x > 0) paths[current.x][current.y] = 'right';
+        if (dir.y > 0) paths[current.x][current.y] = 'down';
     }
 }
 
-function initEnemies() {
+// Find an empty tile on the map
+function getEmpty(myMap) {
+    while (true) {
+        var col = floor(random(cols));
+        var row = floor(random(rows));
+        if (myMap[col][row] === 0) return {x: col, y: row};
+    }
+}
+
+// Find tower at specific column and row
+function getTower(col, row) {
+    for (var i = 0; i < towers.length; i++) {
+        var t = towers[i];
+        if (t.pos.x === col && t.pos.y === row) return t;
+    }
+    return null;
+}
+
+// Generate map indicating walkability of each tile (0 = walkable)
+function walkable() {
+    var walkable = copyMap(grid);
+    for (var i = 0; i < towers.length; i++) {
+        var t = towers[i];
+        walkable[t.pos.x][t.pos.y] = 1;
+    }
+    return walkable;
+}
+
+function initEntities() {
     enemies = [];
-    enemiesStack = [];
+    newEnemies = [];
+    towers = [];
+    newTowers = [];
 }
 
 function resetGame() {
+    initEntities();
     generateMap();
-    initEnemies();
     paused = true;
     maxHealth = 100;
     health = maxHealth;
@@ -67,17 +143,18 @@ function resetGame() {
 }
 
 // Sets tile width and height based on canvas size and map dimensions
-function resizeTiles(cols, rows) {
-    tWidth = width / cols;
-    tHeight = height / rows;
+function resizeTiles() {
+    tw = width / cols;
+    th = height / rows;
 }
 
-// Update game status
+// Update status display
 function updateStatus() {
     document.getElementById('wave').innerHTML = 'Wave ' + wave;
     document.getElementById('health').innerHTML = health + '/' + maxHealth;
     document.getElementById('cash').innerHTML = '$' + cash;
 }
+
 
 // Main p5 functions
 
@@ -89,7 +166,7 @@ function setup() {
     canvas.parent('sketch-holder');
     resizeCanvas(w, div.offsetHeight, true);
     // Setup proper tile size
-    resizeTiles(cols, rows);
+    resizeTiles();
     // Initialize
     resetGame();
 }
@@ -97,7 +174,123 @@ function setup() {
 function draw() {
     background(0);
 
-    // Process tiles
+    // Empty tiles and walls
+    for (var col = 0; col < cols; col++) {
+        for (var row = 0; row < rows; row++) {
+            var t = grid[col][row];
+            if (t === 0 && gridMode) {
+                noFill();
+                stroke(255, 31);
+                rect(col * tw, row * th, tw, th);
+            } else if (t === 1) {
+                fill(1, 50, 67);
+                gridMode ? stroke(255, 31) : stroke(255, 63);
+                rect(col * tw, row * th, tw, th);
+            }
+        }
+    }
+
+    // Spawnpoints
+    for (var i = 0; i < spawnpoints.length; i++) {
+        var t = spawnpoints[i];
+        fill(0, 230, 64);
+        stroke(255);
+        rect(t.x * tw, t.y * th, tw, th);
+    }
+
+    // Exit
+    fill(207, 0, 15);
+    stroke(255);
+    rect(exit.x * tw, exit.y * th, tw, th);
+}
+
+
+// User input
+
+function keyPressed() {
+    switch (keyCode) {
+        case 17:
+            // Ctrl
+            gridMode = !gridMode;
+            break;
+        case 37:
+            // Left arrow
+            if (cols !== minCols) {
+                cols--;
+                resizeTiles();
+                resetGame();
+            }
+            break;
+        case 38:
+            // Up arrow
+            if (rows !== minRows) {
+                rows--;
+                resizeTiles();
+                resetGame();
+            }
+            break;
+        case 39:
+            // Right arrow
+            cols++;
+            resizeTiles();
+            resetGame();
+            break;
+        case 40:
+            // Down arrow
+            rows++;
+            resizeTiles();
+            resetGame();
+            break;
+    }
+}
+
+/*
+// Misc functions
+// Generate map of ideal path to exit tile
+function generatePathMap(walkMap, col, row) {
+    var frontier = new Queue();
+    var pos = coordsToString(col, row);
+    frontier.enqueue(pos);
+    var cameFrom = {};
+    cameFrom[pos] = null;
+    
+    while (!frontier.isEmpty()) {
+        var current = frontier.dequeue();
+        pos = stringToCoords(current);
+        var neighbors = getNeighbors(walkMap, pos.x, pos.y);
+        
+        for (var i = 0; i < neighbors.length; i++) {
+            var next = neighbors[i];
+            if (!(next in cameFrom)) {
+                frontier.enqueue(next);
+                cameFrom[next] = current;
+            }
+        }
+        
+    }
+
+    return cameFrom;
+}
+
+// Generate map of walkability for each tile
+function generateWalkMap() {
+    var walkMap = [];
+    for (var col = 0; col < cols; col++) {
+        walkMap[col] = [];
+        for (var row = 0; row < rows; row++) {
+            walkMap[col][row] = grid[col][row].canWalk;
+        }
+    }
+    return walkMap;
+}
+
+
+// Main p5 functions
+
+function draw() {
+    background(0);
+
+    // Tiles
     for (var col = 0; col < cols; col++) {
         for (var row = 0; row < rows; row++) {
             var tile = grid[col][row];
@@ -105,9 +298,41 @@ function draw() {
             tile.draw();
         }
     }
+
+    // Enemies
+    for (var i = 0; i < enemies.length; i++) {
+        var e = enemies[i];
+        e.update();
+        e.draw();
+        var t = getCoords(e.pos.x, e.pos.y);
+        var c = coordsToString(t.col, t.row);
+        if (pathMap[c] !== null) {
+            var next = stringToCoords(pathMap[c]);
+            var center = getCenter(next.x, next.y);
+            e.pos = createVector(center.x, center.y);
+        }
+    }
     
     updateStatus();
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO generate walk map whenever a tile is changed, only update changed tile to be more efficient
 
 /*
 var enemies;
@@ -153,13 +378,6 @@ function resetMap() {
     updateStatus();
 }
 
-// Update game status
-function updateStatus() {
-    document.getElementById('name').innerHTML = meta.name;
-    document.getElementById('wave').innerHTML = 'Wave ' + wave;
-    document.getElementById('health').innerHTML = health + '/' + meta.health;
-    document.getElementById('cash').innerHTML = '$' + cash;
-}
 
 
 // Main p5 functions
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,28 +1,73 @@
+// Build 2d array of value
+function buildMap(cols, rows, value) {
+    var arr = [];
+    for (var col = 0; col < cols; col++) {
+        arr[col] = [];
+        for (var row = 0; row < rows; row++) {
+            arr[col][row] = value;
+        }
+    }
+    return arr;
+}
+
+// Get center of tile at coords
+function getCenter(col, row) {
+    return {x: col*tw + tw/2, y: col*th + th/2};
+}
+
+// Copy 2d array
+function copyMap(myMap) {
+    var copy = [];
+    for (var col = 0; col < myMap.length; col++) {
+        copy[col] = [];
+        for (var row = 0; row < myMap[0].length; row++) {
+            copy[col][row] = myMap[col][row];
+        }
+    }
+    return copy;
+}
+
+// Convert coords to a string
+function cts(col, row) {
+    return col + ',' + row;
+}
+
 // Return column and row
 function getCoords(x, y) {
-    return {col: floor(x / tWidth), row: floor(y / tHeight)};
+    return {x: floor(x / tw), y: floor(y / th)};
 }
 
-/*
-function getCenter(col, row) {
-    return {
-        x: col * tileWidth + tileWidth / 2,
-        y: row * tileHeight + tileHeight / 2
-    };
+// Get non-diagonal walkable neighbors in a rectangular array
+function getNeighbors(walkMap, x, y) {
+    var neighbors = [];
+    if (x !== 0 && walkMap[x - 1][y] === 0) {
+        neighbors.push(cts(x - 1, y));
+    }
+    if (y !== 0 && walkMap[x][y - 1] === 0) {
+        neighbors.push(cts(x, y - 1));
+    }
+    if (x !== walkMap.length - 1 && walkMap[x + 1][y] === 0) {
+        neighbors.push(cts(x + 1, y));
+    }
+    if (y !== walkMap[x].length - 1 && walkMap[x][y + 1] === 0) {
+        neighbors.push(cts(x, y + 1));
+    }
+    return neighbors;
 }
 
-// If attr is undefined, return default value, otherwise return attr
-function getDefault(attr, value) {
-    return typeof attr === 'undefined' ? value : attr;
+// Convert obj to vector
+function otv(obj) {
+    return createVector(obj.x, obj.y);
 }
 
-// Find dimensions of a rectangular nested array
-function getDimensions(arr) {
-    return {cols: arr[0].length, rows: arr.length};
+// Convert string to coords
+function stc(str) {
+    var arr = str.split(',');
+    return {x: parseInt(arr[0]), y: parseInt(arr[1])};
 }
 
-function getTile(x, y) {
-    var p = getGridCoords(x, y);
-    return palette[grid[p.row][p.col]];
+// Convert string to p5.Vector
+function stv(str) {
+    var pos = stc(str);
+    return createVector(pos.x, pos.y);
 }
-*/
